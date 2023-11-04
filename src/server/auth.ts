ead83a4c70c8d3c09af65cdc52fb2667b0d78cf3
elvis-ndubuisi <provictor.ie@gmail.com>
2023-11-04 12:42:57 +0100

    refactor: :art: just a commit

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,10 +1,13 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import {
-  getServerSession,
-  type DefaultSession,
-  type NextAuthOptions,
+	getServerSession,
+	type DefaultSession,
+	type NextAuthOptions,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import EmailProvider from "next-auth/providers/email";
+import FacebookProvider from "next-auth/providers/facebook";
+import GmailProvider from "next-auth/providers/google";
 
 import { env } from "@/env.mjs";
 import { db } from "@/server/db";
@@ -16,18 +19,18 @@ import { db } from "@/server/db";
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
 declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
+	interface Session extends DefaultSession {
+		user: {
+			id: string;
+			// ...other properties
+			// role: UserRole;
+		} & DefaultSession["user"];
+	}
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+	// interface User {
+	//   // ...other properties
+	//   // role: UserRole;
+	// }
 }
 
 /**
@@ -36,31 +39,45 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
-  },
-  adapter: PrismaAdapter(db),
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
+	callbacks: {
+		session: ({ session, user }) => ({
+			...session,
+			user: {
+				...session.user,
+				id: user.id,
+			},
+		}),
+	},
+	adapter: PrismaAdapter(db),
+	providers: [
+		DiscordProvider({
+			clientId: env.DISCORD_CLIENT_ID,
+			clientSecret: env.DISCORD_CLIENT_SECRET,
+		}),
+		EmailProvider({
+			server: {
+				host: process.env.EMAIL_SERVER_HOST,
+				port: process.env.EMAIL_SERVER_PORT,
+				auth: {
+					user: process.env.EMAIL_SERVER_USER,
+					pass: process.env.EMAIL_SERVER_PASSWORD,
+				},
+			},
+			from: process.env.EMAIL_FROM,
+		}),
+		FacebookProvider({
+			// clientId: process.env.FACEBOOK_CLIENT_ID,
+			// clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+			clientId: "",
+			clientSecret: "",
+		}),
+		GmailProvider({
+			// clientId: process.env.GOOGLE_CLIENT_ID,
+			// clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			clientId: "",
+			clientSecret: "",
+		}),
+	],
 };
 
 /**
