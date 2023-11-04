b3a073e21eb432db5e80850f065dea198d023485
elvis-ndubuisi <provictor.ie@gmail.com>
2023-11-04 11:45:41 +0100

    chore: updated new env(s)

--- src/env.mjs
+++ src/env.mjs
@@ -11,7 +11,7 @@ export const env = createEnv({
 			.string()
 			.url()
 			.refine(
-				(str) => !str.includes("YOUR_MYSQL_URL_HERE"),
+				(str) => !str.includes("YOUR_MongoDB_URL_HERE"),
 				"You forgot to change the default URL",
 			),
 		NODE_ENV: z
@@ -28,9 +28,13 @@ export const env = createEnv({
 			// VERCEL_URL doesn't include `https` so it cant be validated as a URL
 			process.env.VERCEL ? z.string() : z.string().url(),
 		),
+		FACEBOOK_CLIENT_ID: z.string(),
+		FACEBOOK_CLIENT_SECRET: z.string(),
+		GOOGLE_CLIENT_ID: z.string(),
+		GOOGLE_CLIENT_SECRET: z.string(),
 		// Add ` on ID and SECRET if you want to make sure they're not empty
-		// DISCORD_CLIENT_ID: z.string(),
-		// DISCORD_CLIENT_SECRET: z.string(),
+		DISCORD_CLIENT_ID: z.string(),
+		DISCORD_CLIENT_SECRET: z.string(),
 	},
 
 	/**
@@ -53,6 +57,10 @@ export const env = createEnv({
 		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 		DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
 		DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+		FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
+		FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
+		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+		GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 	},
 	/**
 	 * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
