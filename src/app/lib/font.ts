4c20ad321d0f1b59fcf8d6e8892fb35806450653
elvis-ndubuisi <provictor.ie@gmail.com>
2023-11-08 13:05:21 +0100

    style: :sparkles: added vercel geist font

---
+++ src/app/lib/font.ts
@@ -0,0 +1,9 @@
+import { GeistSans } from "geist/font";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-sans",
+});
+
+export { GeistSans as geist, inter };
