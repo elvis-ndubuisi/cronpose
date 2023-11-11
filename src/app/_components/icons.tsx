588ef614351a35ae3e3e1bf8c0d146c24e6b3006
elvis-ndubuisi <provictor.ie@gmail.com>
2023-11-11 08:53:15 +0100

    chore: add external icons

--- src/app/_components/icons.tsx
+++ src/app/_components/icons.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { cn } from "../lib/utils";
 
 export function Google() {
@@ -49,6 +50,348 @@ export function Close() {
 				strokeWidth='2'
 				d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
 			/>
+		</svg>
+	);
+}
+
+export function Dashboard() {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			className='mr-2 h-4 w-4'>
+			<rect
+				width='7'
+				height='7'
+				x='3'
+				y='3'
+				rx='1'
+			/>
+			<rect
+				width='7'
+				height='7'
+				x='14'
+				y='3'
+				rx='1'
+			/>
+			<rect
+				width='7'
+				height='7'
+				x='14'
+				y='14'
+				rx='1'
+			/>
+			<rect
+				width='7'
+				height='7'
+				x='3'
+				y='14'
+				rx='1'
+			/>
+		</svg>
+	);
+}
+
+export function Flag() {
+	return (
+		<svg
+			className=' h-8 w-8 text-gray-700 dark:text-gray-200'
+			fill='none'
+			height='24'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			viewBox='0 0 24 24'
+			width='24'
+			xmlns='http://www.w3.org/2000/svg'>
+			<path d='M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z' />
+			<line
+				x1='4'
+				x2='4'
+				y1='22'
+				y2='15'
+			/>
+		</svg>
+	);
+}
+
+export function Icon00() {
+	return (
+		<svg
+			className=' h-6 w-6 text-gray-700 dark:text-gray-200'
+			fill='none'
+			height='24'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			viewBox='0 0 24 24'
+			width='24'
+			xmlns='http://www.w3.org/2000/svg'>
+			<rect
+				height='8'
+				rx='2'
+				width='8'
+				x='2'
+				y='2'
+			/>
+			<path d='M14 2c1.1 0 2 .9 2 2v4c0 1.1-.9 2-2 2' />
+			<path d='M20 2c1.1 0 2 .9 2 2v4c0 1.1-.9 2-2 2' />
+			<path d='M10 18H5c-1.7 0-3-1.3-3-3v-1' />
+			<polyline points='7 21 10 18 7 15' />
+			<rect
+				height='8'
+				rx='2'
+				width='8'
+				x='14'
+				y='14'
+			/>
+		</svg>
+	);
+}
+
+export function Mail() {
+	return (
+		<svg
+			className='mr-2 h-4 w-4 text-gray-700 dark:text-gray-200'
+			fill='none'
+			height='24'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			viewBox='0 0 24 24'
+			width='24'
+			xmlns='http://www.w3.org/2000/svg'>
+			<polyline points='22 12 16 12 14 15 10 15 8 12 2 12' />
+			<path d='M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z' />
+		</svg>
+	);
+}
+
+export function Settings() {
+	return (
+		<svg
+			className='mr-2 h-5 w-5 text-gray-700 dark:text-gray-200'
+			fill='none'
+			height='24'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			viewBox='0 0 24 24'
+			width='24'
+			xmlns='http://www.w3.org/2000/svg'>
+			<path d='M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z' />
+			<circle
+				cx='12'
+				cy='12'
+				r='3'
+			/>
+		</svg>
+	);
+}
+
+export function Send() {
+	return (
+		<svg
+			className=' h-6 w-6 text-gray-700 dark:text-gray-200'
+			fill='none'
+			height='24'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			viewBox='0 0 24 24'
+			width='24'
+			xmlns='http://www.w3.org/2000/svg'>
+			<path d='m22 2-7 20-4-9-9-4Z' />
+			<path d='M22 2 11 13' />
+		</svg>
+	);
+}
+
+export function Tweak() {
+	return (
+		<svg
+			className=' h-6 w-6 text-gray-700 dark:text-gray-200'
+			fill='none'
+			height='24'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			viewBox='0 0 24 24'
+			width='24'
+			xmlns='http://www.w3.org/2000/svg'>
+			<circle
+				cx='18'
+				cy='18'
+				r='3'
+			/>
+			<circle
+				cx='6'
+				cy='6'
+				r='3'
+			/>
+			<path d='M18 6V5' />
+			<path d='M18 11v-1' />
+			<line
+				x1='6'
+				x2='6'
+				y1='9'
+				y2='21'
+			/>
+		</svg>
+	);
+}
+
+export function MailBox(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			stroke-width='1.5'
+			stroke-linecap='round'
+			stroke-linejoin='round'
+			{...props}>
+			<path d='M22 17a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9.5C2 7 4 5 6.5 5H18c2.2 0 4 1.8 4 4v8Z' />
+			<polyline points='15,9 18,9 18,11' />
+			<path d='M6.5 5C9 5 11 7 11 9.5V17a2 2 0 0 1-2 2v0' />
+			<line
+				x1='6'
+				x2='7'
+				y1='10'
+				y2='10'
+			/>
+		</svg>
+	);
+}
+
+export function MailWarning(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			stroke-width='1.5'
+			stroke-linecap='round'
+			stroke-linejoin='round'
+			{...props}>
+			<path d='M22 10.5V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h12.5' />
+			<path d='m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7' />
+			<path d='M20 14v4' />
+			<path d='M20 22v.01' />
+		</svg>
+	);
+}
+
+export function SendHorizontal(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			stroke-width='1.5'
+			stroke-linecap='round'
+			stroke-linejoin='round'
+			{...props}>
+			<path d='m3 3 3 9-3 9 19-9Z' />
+			<path d='M6 12h16' />
+		</svg>
+	);
+}
+
+export function Trash(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			stroke-width='1.5'
+			stroke-linecap='round'
+			stroke-linejoin='round'
+			{...props}>
+			<path d='M3 6h18' />
+			<path d='M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6' />
+			<path d='M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2' />
+			<line
+				x1='10'
+				x2='10'
+				y1='11'
+				y2='17'
+			/>
+			<line
+				x1='14'
+				x2='14'
+				y1='11'
+				y2='17'
+			/>
+		</svg>
+	);
+}
+
+export function ScrollText(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			stroke-width='1.5'
+			stroke-linecap='round'
+			stroke-linejoin='round'
+			{...props}>
+			<path d='M8 21h12a2 2 0 0 0 2-2v-2H10v2a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v3h4' />
+			<path d='M19 17V5a2 2 0 0 0-2-2H4' />
+			<path d='M15 8h-5' />
+			<path d='M15 12h-5' />
+		</svg>
+	);
+}
+
+export function Users(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			stroke-width='1.5'
+			stroke-linecap='round'
+			stroke-linejoin='round'
+			{...props}>
+			<path d='M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2' />
+			<circle
+				cx='9'
+				cy='7'
+				r='4'
+			/>
+			<path d='M22 21v-2a4 4 0 0 0-3-3.87' />
+			<path d='M16 3.13a4 4 0 0 1 0 7.75' />
 		</svg>
 	);
 }
