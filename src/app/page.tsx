36593e9a43991936bc308256ef3c48a9a9892804
elvis-ndubuisi <provictor.ie@gmail.com>
2023-11-13 14:13:46 +0100

    feat: :lipstick: setup shadcn ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
+import { Button } from "./_components/ui/button";
 
 export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
-  const session = await getServerAuthSession();
-  return <main className="min-h-screen"></main>;
+	const hello = await api.post.hello.query({ text: "from tRPC" });
+	const session = await getServerAuthSession();
+	return (
+		<main className="min-h-screen">
+			<Button variant={"destructive"}>asdfads</Button>
+		</main>
+	);
 }
