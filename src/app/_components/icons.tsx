db49a4200a9aff7d8caaa2fb7fa1f63ead3907f4
elvis-ndubuisi <provictor.ie@gmail.com>
2023-11-14 20:52:44 +0100

    chore: :lipstick: added new icon set

--- src/app/_components/icons.tsx
+++ src/app/_components/icons.tsx
@@ -77,10 +77,10 @@ export function Dashboard() {
 	);
 }
 
-export function Flag() {
+export function Flag(props: React.SVGProps<SVGSVGElement>) {
 	return (
 		<svg
-			className=" h-8 w-8 text-gray-700 dark:text-gray-200"
+			{...props}
 			fill="none"
 			height="24"
 			stroke="currentColor"
@@ -371,6 +371,49 @@ export function Search(props: React.SVGProps<SVGSVGElement>) {
 		>
 			<circle cx="11" cy="11" r="8" />
 			<path d="m21 21-4.3-4.3" />
+		</svg>
+	);
+}
+
+export function Tag(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			{...props}
+		>
+			<path d="M12 2H2v10l9.29 9.29c.94.94 2.48.94 3.42 0l6.58-6.58c.94-.94.94-2.48 0-3.42L12 2Z" />
+			<path d="M7 7h.01" />
+		</svg>
+	);
+}
+
+export function Container(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			{...props}
+		>
+			<path d="M22 7.7c0-.6-.4-1.2-.8-1.5l-6.3-3.9a1.72 1.72 0 0 0-1.7 0l-10.3 6c-.5.2-.9.8-.9 1.4v6.6c0 .5.4 1.2.8 1.5l6.3 3.9a1.72 1.72 0 0 0 1.7 0l10.3-6c.5-.3.9-1 .9-1.5Z" />
+			<path d="M10 21.9V14L2.1 9.1" />
+			<path d="m10 14 11.9-6.9" />
+			<path d="M14 19.8v-8.1" />
+			<path d="M18 17.5V9.4" />
 		</svg>
 	);
 }
